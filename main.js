392bb9f44d3e539b13051a5bacfd1e8806a0c9d9
AndreaPanzeriDev <andpanz@libero.it>
2022-11-03 17:37:54 +0100

    conclusione

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ let arrayNumber = [];
 
 let guessNumber = [];
 
-let seconds= 10 * 1000; //change the five with the seconds that you want
-
+let seconds = 10; //change the five with the seconds that you want
+let rseconds = seconds * 1000;
 
 //max number that you can display 
 const max_n = 100;
@@ -21,29 +21,35 @@ const guess_n = 5;
 //diplay number
 let display_number = document.getElementById("numberOutput");
 
+//set c
+let c = seconds;
+
 
 
 //inizio game
-function Play(){
+function Play() {
+    c == seconds;
     //clean the previus number
     display_number.innerHTML = ' ';
+    arrayNumber = [];
+    guessNumber = [];
     randomNumber();
-    setTimeout(clean, seconds);
+    setInterval(countDown, 1000);
+    setTimeout(clean, rseconds);
     //create another setTimout and put +1 second because the cicle for is to fast (FASTT BOYYYYYYYYYY)
-    setTimeout(Ask, seconds + 1000);
-
+    setTimeout(Ask, rseconds + 1000);
 }
 
 
 
-function randomNumber(){
+function randomNumber() {
     //generate guess_n random number
-    for(let i = 0; i < guess_n; i++){
+    for (let i = 0; i < guess_n; i++) {
         let number = Math.round(Math.random() * max_n);
         //check if the number is already in the array
-        if(arrayNumber.includes(number)){
+        if (arrayNumber.includes(number)) {
             i--;
-        }else{
+        } else {
             //push the number into the array
             arrayNumber.push(number);
             //display the number on the html page
@@ -51,21 +57,42 @@ function randomNumber(){
         }
     }
     console.log(arrayNumber);
-    
+
 }
 
 
-function clean(){
+function clean() {
     display_number.innerHTML = ' ';
 
 }
 
 
-function Ask(){
+function Ask() {
     //ask
-    for(let i=0; i<guess_n; i++){
+    for (let i = 0; i < guess_n; i++) {
         let input = parseInt(prompt("Inserisci il " + (i + 1) + "° numero"))
         guessNumber.push(input);
     }
     console.log(guessNumber);
+    Check();
+}
+
+//create a countdown
+function countDown() {
+    let output = document.getElementById("countdown");
+    output.innerHTML = ' ';
+    console.log(c);
+    output.innerHTML = c;
+    c--;
+}
+
+
+function Check(){
+    let count = 0;
+    for(let i=0; i < guess_n; i++){
+        if(arrayNumber[i] == guessNumber[i]){
+            count++;
+        }
+    }
+    alert("Hai inserito " + count + " risposte giuste. \nI numeri da inserire erano: " +  arrayNumber + "\nMentre i numeri che hai inserito sono: " + guessNumber);
 }
