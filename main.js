99a5b32434cc69b23791016793905e1879c3be04
AndreaPanzeriDev <andpanz@libero.it>
2022-11-03 16:09:10 +0100

    play function || random number function

--- main.js
+++ main.js
@@ -5,3 +5,40 @@ Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 */
+
+let arrayNumber = [];
+
+let guessNumber = [];
+
+let second = 5 * 1000; //change the five with the seconds that you want
+
+
+//max number that you can display 
+const max_n = 100;
+//max number the player have to memorize
+const guess_n = 5;
+//inizio game
+function Play(){
+    randomNumber();
+
+}
+
+
+
+function randomNumber(){
+    for(let i = 0; i < 5; i++){
+        let number = Math.round(Math.random() * max_n);
+        if(arrayNumber.includes(number)){
+            i--;
+        }else{
+            arrayNumber.push(number);
+        }
+    }
+    console.log(arrayNumber);
+    
+}
+
+
+function displayNumber(){
+    
+}
